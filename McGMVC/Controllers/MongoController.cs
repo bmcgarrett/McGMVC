1d9a91acd9a89427cb05369db56afe07d063ceb1
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-22 11:08:01 -0400

    Update layouts view

---
+++ McGMVC/Controllers/MongoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using McGMVC.Models;
+
+namespace McGMVC.Controllers
+{
+    public class MongoController : Controller
+    {
+        //
+        // GET: /Mongo/
+
+        public ActionResult Index()
+        {
+            var model1 = new Book();
+            model1._id = 08898933;
+            model1.Title = "Node.JS in Action";
+            model1.Author = "McGarrett, Brendan";
+            var model2 = new Book();
+            model2._id = 08523424;
+            model2.Title = "SharePoint 2013";
+            model2.Author = "Doe, John";
+            var books = new List<Book>();
+            books.Add(model1);
+            books.Add(model2);
+            return View(books);
+        }
+
+    }
+}
---
+++ McGMVC/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace McGMVC.Models
+{
+    public class Book
+    {
+        public int _id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
