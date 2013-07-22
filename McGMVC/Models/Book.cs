66cb3b933a21fb2b62accd0a1d6a777dd5809220
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-22 16:05:54 -0400

    Updated Book class

--- McGMVC/Models/Book.cs
+++ McGMVC/Models/Book.cs
@@ -8,7 +8,6 @@ namespace McGMVC.Models
 {
     public class Book
     {
-        public int _id { get; set; }
         public string title { get; set; }
         public string author { get; set; }
     }
