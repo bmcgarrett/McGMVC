3aa27c011d2f13649d93c30ffc74bec1f5d15ca2
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-23 21:58:50 -0400

    Updated layouts

--- McGMVC/App_Start/BundleConfig.cs
+++ McGMVC/App_Start/BundleConfig.cs
@@ -20,6 +20,8 @@ namespace McGMVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/Bootstrap/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/McGMVC").Include("~/Scripts/McGMVC/mcg.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ McGMVC/Scripts/McGMVC/mcg.js
@@ -0,0 +1,5 @@
+﻿
+$(document).ready(function() {
+    $('.navbar li').removeClass('active');
+    $('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
+});
