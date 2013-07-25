ed46c8cf135f3cd7415023e6fb06cd267826c866
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-24 22:10:37 -0400

    Removed jQuery Alert

--- McGMVC/Scripts/McGMVC/mcg.js
+++ McGMVC/Scripts/McGMVC/mcg.js
@@ -17,9 +17,6 @@ $(document).ready(function () {
 
     $(document).on('click','#delBookMongoBtn', function () {
         var bookID = $(this).parent().parent().attr('id');
-        //var bTitle = $(this).parent().parent().children("#bTitleRow").val();
-        //var bAuthor = $(this).parent().parent().children("#bAuthorRow").val();
-        //alert(bookID + bTitle + bAuthor);
         $.get("/mongo/delete", { id: bookID }).done(function () {
             window.location.href = "/mongo";
         });
@@ -36,7 +33,6 @@ $(document).ready(function () {
     $(document).on('click', '#saveBtnEditBookMongo', function () {
         updateBookTitle = $('#bookTitleInputEdit').val();
         updateBookAuthor = $('#bookAuthorInputEdit').val();
-        alert(bookIDMongo + updateBookTitle + updateBookAuthor);
         $.get("/mongo/edit", { id: bookIDMongo, title: updateBookTitle, author: updateBookAuthor }).done(function() {
             $('#editBookModal').modal("hide");
             window.location.href = "/mongo";
