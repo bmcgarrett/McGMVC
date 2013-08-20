85dd9771c33b1c6904f63bf32e320074d4f47d0e
Brendan McGarrett <bmcgarrett@gmail.com>
2013-08-20 10:15:48 -0400

    Updated Solution with EF

---
+++ McGMVC/DAL/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using McGMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace McGMVC.DAL
+{
+    public class BookContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
